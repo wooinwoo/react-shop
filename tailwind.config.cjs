2f58ada246af96f04e7028b725cedfbf3dd65064
wooinwoo <wooin1001@gmail.com>
2022-10-23 02:48:26 +0900

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1280px",
+      xl2: "1360px",
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["emerald", "dark", "forest", "synthwave"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
