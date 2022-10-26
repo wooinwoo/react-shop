b5e274feee2d713469dbc0f9ba6076729b358f4f
wooinwoo <wooin1001@gmail.com>
2022-10-26 11:31:20 +0900

    mod : route -> 1. "/fation" 2. "/accessory" 3. "/digital" 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import GetProductList from "./api/GetProductList";
 import Header from "./Header";
-import Main from "./pages/Main";
+import MainPage from "./pages/MainPage";
+import CategoryPage from "./pages/CategoryPage";
 import Footer from "./Footer";
 
 function App() {
@@ -13,7 +14,16 @@ function App() {
         <Header />
         <section className="pt-16">
           <Routes>
-            <Route path="/" element={<Main />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/fation" element={<CategoryPage keyword="패션" />} />
+            <Route
+              path="/accessory"
+              element={<CategoryPage keyword="액세서리" />}
+            />
+            <Route
+              path="/digital"
+              element={<CategoryPage keyword="디지털" />}
+            />
           </Routes>
         </section>
         <Footer />
