d49949801dbe92af1f7791210acfd3ee99188c10
wooinwoo <wooin1001@gmail.com>
2022-10-28 06:51:01 +0900

    Mod: sideDrawer 수정

--- src/components/SideDrawer.tsx
+++ src/components/SideDrawer.tsx
@@ -1,33 +1,43 @@
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { drawerState } from "../state/State";
 
 const SideDrawer = () => {
+  const [drawer, setDrawer] = useRecoilState(drawerState);
+  const categoryLink: Array<Array<string>> = [
+    ["패션", "/fashion"],
+    ["악세서리", "/accessory"],
+    ["디지털", "/digital"],
+  ];
   return (
-    <div className="drawer-side">
-      {/* <label for="side-menu" classNamelass="drawer-overlay"></label> */}
-      <ul className="menu w-60 sm:w-80 p-4 overflow-y-auto bg-white dark:bg-base-100">
-        <li>
-          <Link
-            className="!text-gray-700 active:!text-white dark:!text-white"
-            to="/fashion">
-            패션
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="!text-gray-700 active:!text-white dark:!text-white"
-            to="/accessory">
-            액세서리
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="!text-gray-700 active:!text-white dark:!text-white"
-            to="/digital">
-            디지털
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div
+        className={
+          drawer
+            ? "fixed top-0 right-[100vw] z-50 translate-x-[70vw] duration-300 md:hidden"
+            : "fixed top-0 right-[100vw] z-50 translate-x-[0vw] duration-300 md:hidden"
+        }>
+        <ul className="menu w-[70vw] h-[100vh]  p-4 overflow-y-auto bg-white dark:bg-base-100">
+          {categoryLink.map((data, idx) => (
+            <li key={idx}>
+              <Link
+                className="!text-gray-700 active:!text-white dark:!text-white"
+                to={data[1]}
+                onClick={() => setDrawer(!drawer)}>
+                {data[0]}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div
+        className={
+          drawer
+            ? "fixed top-0 z-40 right-0 w-[100vw] h-[100vh] bg-black opacity-50 md:hidden"
+            : ""
+        }
+        onClick={() => setDrawer(!drawer)}></div>
+    </>
   );
 };
 
