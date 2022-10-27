005c485785a1b9b011e66871ad1f353ded686cf8
wooinwoo <wooin1001@gmail.com>
2022-10-28 06:50:45 +0900

    Mod: DrawerBtn 수정

--- src/components/DrawerToggleButton.tsx
+++ src/components/DrawerToggleButton.tsx
@@ -1,8 +1,11 @@
+import { useRecoilState } from "recoil";
+import { drawerState } from "../state/State";
 const DrawerToggle = () => {
+  const [drawer, setDrawer] = useRecoilState(drawerState);
   return (
-    <label
-      for="side-menu"
-      className="flex-none lg:hidden btn btn-square btn-ghost w-10 sm:w-auto">
+    <div
+      className=" md:hidden flex-none btn btn-square btn-ghost w-10 sm:w-auto"
+      onClick={() => setDrawer(!drawer)}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -14,7 +17,7 @@ const DrawerToggle = () => {
           stroke-width="2"
           d="M4 6h16M4 12h16M4 18h16"></path>
       </svg>
-    </label>
+    </div>
   );
 };
 
