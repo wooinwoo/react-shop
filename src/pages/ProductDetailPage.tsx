64015185a97e335b7190f1c7426e899986986cf9
wooinwoo <wooin1001@gmail.com>
2022-10-31 17:19:28 +0900

    Mod: 타입 수정

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { productListState } from "../state/State";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { productListState, cartState } from "../state/State";
 import Breadcrumbs from "../components/Breadcrumbs";
+import StarRating from "../components/StarRating";
+
+import { CreateCartItem } from "../helpers/CartHelpers";
 
 interface dataProps {
   id: number;
@@ -19,6 +22,7 @@ interface dataProps {
 const ProductDetailPage = () => {
   const url = window.location.href.split("/");
   const dataList: Array<dataProps> = useRecoilValue(productListState);
+  const [cart, setCart]: any = useRecoilState(cartState);
 
   const filterData = dataList.filter(
     (d) => String(d.id) === url[url.length - 1]
@@ -37,9 +41,6 @@ const ProductDetailPage = () => {
         },
         title: "",
       };
-
-  console.log(item);
-
   const category: any = {
     initial: "1",
     "men's clothing": "패션",
@@ -53,10 +54,10 @@ const ProductDetailPage = () => {
       <section className="pt-4 lg:pt-5 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
         <Breadcrumbs depth1={category[item.category]} depth2={item.title} />
         <div className="lg:flex lg:items-center mt-6 md:mt-14 px-2 lg:px-0">
-          <figure className="flex-shrink-0 rounded-2xl overflow-hidden px-4 py-4 bg-white view_image">
+          <figure className="flex-shrink-0 rounded-2xl overflow-hidden px-4 py-4 bg-white">
             <img
               src={item.image}
-              alt="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+              alt={item.title}
               className="object-contain w-full h-72"
             />
           </figure>
@@ -67,35 +68,18 @@ const ProductDetailPage = () => {
             </h2>
             <p>{item.description}</p>
             <div className="flex items-center mt-3">
-              <div className="rating rating-half">
-                {Array(10)
-                  .fill(0)
-                  .map((d, idx) => (
-                    <input
-                      key={idx}
-                      type="radio"
-                      name="rating-10"
-                      className={
-                        idx % 2 === 0
-                          ? "bg-yellow-400 cursor-default mask mask-star-2 mask-half-1"
-                          : "bg-yellow-400 cursor-default mask mask-star-2 mask-half-2"
-                      }
-                      disabled
-                      checked={
-                        Math.floor(item.rating.rate * 2) == idx + 1
-                          ? true
-                          : false
-                      }
-                    />
-                  ))}
-              </div>
+              <StarRating rate={item.rating.rate} />
               <div className="ml-2">
                 {item.rating.rate} / {item.rating.count} 참여
               </div>
             </div>
             <p className="mt-2 mb-4 text-3xl">${item.price}</p>
             <div className="card-actions">
-              <button className="btn btn-primary">장바구니에 담기</button>
+              <button
+                className="btn btn-primary"
+                onClick={() => setCart(CreateCartItem(item, cart ))}>
+                장바구니에 담기
+              </button>
               <Link className="btn btn-outline ml-1" to="/cart">
                 장바구니로 이동
               </Link>
