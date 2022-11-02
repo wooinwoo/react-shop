7b8de5a40c8664c9f7cf7db0f823142b9b8d3532
wooinwoo <wooin1001@gmail.com>
2022-11-02 17:17:33 +0900

    mod: 장바구니 아이템 삭제, 수량 합계 추가

--- src/components/CartTable.tsx
+++ src/components/CartTable.tsx
@@ -1,60 +1,100 @@
 import { useRecoilState } from "recoil";
-import { cartState } from "../state/State";
+import { cartState, checkAllState } from "../state/State";
 
-import { AddCartItem } from "../helpers/CartHelpers";
-import { SubCartItem } from "../helpers/CartHelpers";
-
+import {
+  SubItem,
+  UserInputItemCount,
+  AddItem,
+  DelItem,
+  OnCheckItem,
+} from "../helpers/CartHelpers";
+import { Link } from "react-router-dom";
 interface cartItem {
   id: number;
   title: string;
   imgSrc: string;
   price: number;
   count: number;
+  checked: boolean;
 }
 
 const CartTable = () => {
   const [cart, setCart] = useRecoilState(cartState);
+  const [checkAll, setCheckAll] = useRecoilState(checkAllState);
+
+  const ClickCheckAll = (checkState: boolean) => {
+    let cartData: any = [];
+    cart.map(
+      (item: cartItem) =>
+        (cartData = [
+          ...cartData,
+          {
+            ...item,
+            checked: checkState,
+          },
+        ])
+    );
+    setCart(cartData);
+  };
+
   return (
-    <div className="overflow-x-auto w-full">
-      <table className="table w-full">
+    <div className="overflow-x-auto w-full max-h-[400px] ">
+      <table className="table w-full block group">
         <thead>
           <tr>
-            <th className="max-w-[50px]">
+            <th className="max-w-[50px] sticky top-0 group-first:z-20">
               <label className="flex items-center">
-                <input type="checkbox" className="checkbox mr-2" />
+                <input
+                  type="checkbox"
+                  className="checkbox mr-2"
+                  checked={checkAll}
+                  onClick={() => {
+                    ClickCheckAll(!checkAll);
+                    setCheckAll(!checkAll);
+                  }}
+                />
                 전체선택
               </label>
             </th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th>선택삭제</th>
+            <th className="sticky top-0"></th>
+            <th className="sticky top-0"></th>
+            <th className="sticky top-0"></th>
+            <th className="sticky top-0">
+              <button onClick={() => setCart(DelItem(cart))}>선택삭제</button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {cart.map((item: cartItem) => (
-            <tr>
+          {cart.map((item: cartItem, idx) => (
+            <tr key={idx}>
               <th>
                 <label>
-                  <input type="checkbox" className="checkbox" />
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    checked={item.checked}
+                    onClick={() => setCart(OnCheckItem(item.id, cart))}
+                  />
                 </label>
               </th>
               <td className="px-0">
                 <div className="flex items-center space-x-3 ">
-                  <div className="flex items-center justify-center">
-                    <div className="w-30 h-30 ">
+                  <div className="flex items-center justify-center w-[100px] h-[100px]">
+                    <div className=" ">
                       <figure className="rounded-2xl p-4 bg-white">
-                        <img
-                          src={item.imgSrc}
-                          alt={item.title}
-                          className="object-contain w-10 h-10"
-                        />
+                        <Link to={"/product/" + item.id}>
+                          <img
+                            src={item.imgSrc}
+                            alt={item.title}
+                            className="object-contain w-[50px] h-[50px]"
+                          />
+                        </Link>
                       </figure>
                     </div>
                   </div>
                   <div>
-                    <div className="font-bold w-20 overflow-clip">
-                      {item.title}
+                    <div className="font-bold w-[50px] sm:w-[100px] md:w-[150px] lg:w-[300px] xl:w-[400px] xl2:w-[500px] text-ellipsis overflow-hidden">
+                      <Link to={"/product/" + item.id}>{item.title}</Link>
                     </div>
                   </div>
                 </div>
@@ -63,20 +103,25 @@ const CartTable = () => {
               <td>
                 <button
                   className="btn h-5"
-                  onClick={() => setCart(SubCartItem(item.id, cart))}>
+                  onClick={() => setCart(SubItem(item.id, cart))}>
                   -
                 </button>
                 <input
                   className="input input-bordered text-center w-10 p-0"
                   value={item.count}
+                  onChange={(e) =>
+                    setCart(UserInputItemCount(item.id, cart, e.target.value))
+                  }
                 />
                 <button
                   className="btn h-5"
-                  onClick={() => setCart(AddCartItem(item.id, cart))}>
+                  onClick={() => setCart(AddItem(item.id, cart))}>
                   +
                 </button>
               </td>
-              <th>${item.price * item.count}</th>
+              <th className="w-[120px]">
+                ${parseFloat((item.price * item.count).toFixed(2))}
+              </th>
             </tr>
           ))}
         </tbody>
