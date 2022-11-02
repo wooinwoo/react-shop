a06fea76834092dc1a1a7fe59e62d36ea0e3f9bf
wooinwoo <wooin1001@gmail.com>
2022-11-02 17:18:50 +0900

    mod: css 수정

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -23,10 +23,12 @@ const CartPage = () => {
             담으러 가기
           </Link>
         </div>
-        <div className="lg:flex justify-between mb-20">
+        <div className="flex justify-between mb-20">
           <div></div>
           <div className="self-start shrink-0 flex items-center mt-10 mb-20">
-            <span className="text-xl md:text-2xl">총 : ${totalPrice}</span>
+            <span className="text-xl md:text-2xl">
+              총 : ${parseFloat(totalPrice.toFixed(2))}
+            </span>
             <label className="modal-button btn btn-primary ml-5">
               구매하기
             </label>
