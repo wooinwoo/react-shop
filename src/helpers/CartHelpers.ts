5998a605a03b41ba9dca47c147db08accb28f7e1
wooinwoo <wooin1001@gmail.com>
2022-11-02 17:18:23 +0900

    mod: 장바구니 수량 입력 UserInputItemCount 추가

--- src/helpers/CartHelpers.ts
+++ src/helpers/CartHelpers.ts
@@ -17,12 +17,13 @@ interface cartItem {
   imgSrc: string;
   price: number;
   count: number;
+  checked: boolean;
 }
 
-export const CreateCartItem = (item: item, cart: Array<cartItem>) => {
+export const CreateItem = (item: item, cart: Array<cartItem>) => {
   let cart_copy = [...cart];
   if (cart_copy.some((data) => data.id === item.id)) {
-    return AddCartItem(item.id, cart);
+    return AddItem(item.id, cart);
   } else {
     return [
       {
@@ -31,13 +32,14 @@ export const CreateCartItem = (item: item, cart: Array<cartItem>) => {
         imgSrc: item.image,
         price: item.price,
         count: 1,
+        checked: true,
       },
       ...cart,
     ];
   }
 };
 
-export const SubCartItem = (id: number, cart: Array<cartItem>) => {
+export const SubItem = (id: number, cart: Array<cartItem>) => {
   let cart_copy = [...cart];
   const index = cart_copy.findIndex((i) => i.id == id);
   if (cart_copy[index].count !== 1) {
@@ -49,7 +51,31 @@ export const SubCartItem = (id: number, cart: Array<cartItem>) => {
   return cart_copy;
 };
 
-export const AddCartItem = (id: number, cart: Array<cartItem>) => {
+export const UserInputItemCount = (
+  id: number,
+  cart: Array<cartItem>,
+  value: string
+) => {
+  console.log(Number(value));
+  let cart_copy = [...cart];
+  const index = cart_copy.findIndex((i) => i.id == id);
+  let count;
+  if (Number(value) > 100) {
+    count = 100;
+  } else if (Number(value) < 1) {
+    count = 0;
+  } else {
+    count = Number(value);
+  }
+
+  cart_copy[index] = {
+    ...cart_copy[index],
+    count: count,
+  };
+  return cart_copy;
+};
+
+export const AddItem = (id: number, cart: Array<cartItem>) => {
   let cart_copy = [...cart];
   const index = cart_copy.findIndex((i) => i.id == id);
   if (cart_copy[index].count !== 100) {
@@ -57,11 +83,22 @@ export const AddCartItem = (id: number, cart: Array<cartItem>) => {
       ...cart_copy[index],
       count: cart_copy[index].count + 1,
     };
+  } else {
+    alert("상품당 최대 주문 수량은 100개입니다.");
   }
   return cart_copy;
 };
 
-export const DelCartItem = () => {
-  // const setData = useRecoilState(productListState);
-  // setData(1);
+export const DelItem = (cart: Array<cartItem>) => {
+  return cart.filter((item) => !item.checked);
+};
+
+export const OnCheckItem = (id: number, cart: Array<cartItem>) => {
+  let cart_copy = [...cart];
+  const index = cart_copy.findIndex((i) => i.id == id);
+  cart_copy[index] = {
+    ...cart_copy[index],
+    checked: !cart_copy[index].checked,
+  };
+  return cart_copy;
 };
