207ef7169eee45dce1a39d4af8d0c77750803dbb
wooinwoo <wooin1001@gmail.com>
2022-12-20 23:47:49 +0900

    Update: icon link 수정

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
     <footer className="p-10 footer bg-base-200 text-base-content footer-center">
       <div className="grid grid-flow-col gap-4">
         <a
-          href="https://zero-base.co.kr/"
+          href="https://wiw-portfolio.com/"
           target="_blank"
           className="link link-hover">
-          제로베이스
+          우인우
         </a>
       </div>
       <ul className="flex">
@@ -196,7 +196,7 @@ const Footer = () => {
       </ul>
       <div className="grid grid-flow-col gap-4">
         <a
-          href="https://www.facebook.com/0base"
+          href="https://www.facebook.com/profile.php?id=100015953311884&mibextid=ZbWKwL"
           target="_blank"
           data-tip="facebook"
           className="tooltip">
@@ -212,7 +212,7 @@ const Footer = () => {
           </svg>
         </a>
         <a
-          href="https://www.instagram.com/zerobase.official"
+          href="https://www.instagram.com/in_u___"
           target="_blank"
           data-tip="instagram"
           className="tooltip">
@@ -228,7 +228,7 @@ const Footer = () => {
           </svg>
         </a>
         <a
-          href="https://github.com/oinochoe"
+          href="https://github.com/wooinwoo"
           target="_blank"
           data-tip="github"
           className="tooltip ml-1">
